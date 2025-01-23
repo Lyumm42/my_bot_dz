f2d9feb2280cbd0fa2fdd7adb321dfb25e193771
Dastan Oskonbaev <dastiw1910@gmail.com>
2025-01-23 17:49:59 +0600

    Lesson 2

--- main.py
+++ main.py
@@ -1,36 +1,97 @@
 import asyncio
 import os
 
-from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
+from aiogram import Bot, Dispatcher, Router, F
+from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import Message
 from dotenv import load_dotenv
 
+from states import Survey
+from keyboards import reply_kb, inline_kb
 load_dotenv()
 
 bot_token=os.getenv("TOKEN")
 
 bot = Bot(token=bot_token)
-dp = Dispatcher()
+router = Router()
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer(f"Hello {message.from_user.first_name}")
 
 
-@dp.message(Command("start"))
-async def send_welcome(message: Message):
-    await message.answer(f"Hello {message.from_user.first_name}!")
+@router.message(Command(commands="help"))
+async def cmd_help(message: Message):
+    await message.answer(f"<i>Hello {message.from_user.first_name}! you asked help</i>", parse_mode="HTML")
 
 
-@dp.message(Command("help"))
-async def help_(message: Message):
-    await message.answer(f"Hello {message.from_user.first_name}! you asked help")
+@router.message(Command(commands='inet'))
+async def cmd_inet(message: Message):
+    await message.answer("Вот ссылки", reply_markup=inline_kb)
 
 
-@dp.message()
+@router.message(Command(commands="survey"))
+async def cmd_survey(message: Message, state: FSMContext):
+    await message.answer("Как вас зовут?")
+    await state.set_state(Survey.name)
+
+
+@router.message(Survey.name, F.text)
+async def process_name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Сколько вам лет ?")
+    await state.set_state(Survey.age)
+
+
+@router.message(Survey.age, F.text)
+async def process_age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("Какой ваш любимый цвет ?")
+    await state.set_state(Survey.color)
+
+
+@router.message(Survey.color, F.text)
+async def process_color(message: Message, state: FSMContext):
+    data = await state.get_data()
+    name = data.get("name")
+    age = data.get("age")
+    color = message.text
+    answer_text = f"""
+                    Отлично !!!\n Спасибо за пройденный опрос!!!
+                    Ваш любимый цвет {color}\nВам {age} лет\nВас зовут {name}"""
+    await message.answer(answer_text)
+    await state.clear()
+
+
+# @router.message(F.text == "Hello")
+# async def reply_hello(message: Message):
+#     await message.reply(f"HIII !!!")
+
+
+@router.message(F.text)
 async def reply_text(message: Message):
-    await message.reply(f"You typed {message.text}")
+    if message.text == "Hi":
+        await message.reply("И тебе привет")
+    elif len(message.text) > 20:
+        await message.reply("У тебя слишком длинный текст")
+    elif message.text == "KB":
+        await message.answer("Выберете опцию:", reply_markup=reply_kb)
+    else:
+        await message.reply(f"You typed {message.text}")
+
+
+@router.message(F.photo)
+async def reply_image(message: Message):
+    await message.answer(f"Nice picture! 👍")
+
 
 
 async def main():
     print("Starting bot...")
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
